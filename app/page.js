c2faaa9e13bca1d4a59ccb07dd0ba33a5e74ca5f
kamal <kamalpreets688@gamil.com>
2025-01-21 15:22:17 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Page() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="week-2">Week 2 Assignmengts</Link>
+        <Link href="week-2">Week 2 Assignments</Link>
       </p>
     </main>
   );
