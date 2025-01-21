d13e7a4a4b0a5223edb238d9fc67823dd1bc7f9d
kamal <kamalpreets688@gamil.com>
2025-01-21 14:36:02 -0700

    assignment

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>Week 2 Assignment</h1>
-      <p>KIDDAA</p>
+      <h1>Shopping List</h1>
+      <p>Kamalpreet Singh</p>
       <p>
         Link to week 2:<Link href=" week-2 "> WEEK-2 </Link>
       </p>
