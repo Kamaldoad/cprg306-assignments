05b78901dd07880a46fb508888915ac75a7b6d3e
kamal <kamalpreets688@gamil.com>
2025-01-21 14:42:02 -0700

    git hub link

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function Page() {
       <h1>Shopping List</h1>
       <p>Kamalpreet Singh</p>
       <p>
-        Link to week 2:<Link href=" week-2 "> WEEK-2 </Link>
+        <Link href="https://www.bing.com/ck/a?!&&p=8d1257f5a1858e9b75f2ab10d4b38077bcb549e1b98e193f69c90bc2b4927b92JmltdHM9MTczNzQxNzYwMA&ptn=3&ver=2&hsh=4&fclid=30f4bca5-269a-6b5f-04e1-af5527226a4a&psq=github&u=a1aHR0cHM6Ly9naXRodWIuY29tLw&ntb=1"> 
+        https://github.com </Link>
       </p>
     </div>
   );
