99f2802b75cc3423f2c15d6aa53da7844baa7868
kamal <kamalpreets688@gamil.com>
2025-01-21 14:19:41 -0700

    title components

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import Title from './title.js';
+
+export default function Page() {
+
+  return (
+    <main>
+      <Title />
+      <Title />
+      <p>KIDDAA</p>
+    </main>
+  );
+}
---
+++ app/week-2/title.js
@@ -0,0 +1,5 @@
+export default function Title() {
+  return (
+    <h1>Week 2 Assignment</h1>
+  );
+}
