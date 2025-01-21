9259d45c4dbe0649c7b064bbd2b101a0d026d30e
kamal <kamalpreets688@gamil.com>
2025-01-21 14:31:45 -0700

    new pages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1>Week 2 Assignment</h1>
       <p>KIDDAA</p>
+      <p>
+        Link to week 2:<Link href=" week-2 "> WEEK-2 </Link>
+      </p>
     </div>
   );
 }
