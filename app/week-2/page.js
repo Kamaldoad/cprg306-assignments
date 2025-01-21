d5a97bb772d8bcd246e630166b46f32253a9ad6c
kamal <kamalpreets688@gamil.com>
2025-01-21 14:54:22 -0700

    week 2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,12 +1,13 @@
-import Title from './title.js';
+import StudentInfo from './student-info.js';
 
 export default function Page() {
 
   return (
     <main>
-      <Title />
-      <Title />
-      <p>KIDDAA</p>
+     <div>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+     </div>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+export default function StudentInfo() {
+    return (
+      <main>
+       <div>
+        <h1>Kamalpreet Singh</h1>
+        <p>
+        <Link href="https://github.com/Kamaldoad"> 
+        https://github.com </Link>
+      </p>
+       </div>
+      </main>
+    );
+  }
