3b274170aeaf058eafa530286c79601cabc95b5b
kamal <kamalpreets688@gamil.com>
2025-02-12 10:32:09 -0700

    week-4

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,6 +1,13 @@
-export default function Page() {
- return (
-     <div>Week 3
-     </div>
- );
+import React from 'react';
+import NewItem from './new-item';
+
+
+const Page = () => {
+  return (
+    <div className='w-[100%] h-[100vh] bg-slate-200 text-black text-center flex justify-center items-center'>
+        <NewItem/>
+    </div>
+  )
 }
+
+export default Page
