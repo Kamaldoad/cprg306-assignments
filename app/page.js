158c7a8daae12010368a1ce24bcfd39fda83cdad
kamal <kamalpreets688@gamil.com>
2025-02-12 10:38:40 -0700

    ..

--- app/page.js
+++ app/page.js
@@ -5,8 +5,13 @@ export default function Page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="/week-2">Week 2 Assignments  </Link>
-        <Link href="/week-3">Week 3 Assignments</Link>
+        <Link href="/week-3">Week 3 Assignments </Link>
         <Link href="/week-4">Week 4 Assignments</Link>
+        <Link href="/week-5">Week 4 Assignments</Link>
+        <Link href="/week-6">Week 4 Assignments</Link>
+        <Link href="/week-7">Week 4 Assignments</Link>
+        <Link href="/week-8">Week 4 Assignments</Link>
+        <Link href="/week-9">Week 4 Assignments</Link>
       </p>
     </main>
   );
