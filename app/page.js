c6faa519c5ebc044df89cc2f2b2c65892793ef22
kamal <kamalpreets688@gamil.com>
2025-04-09 09:09:16 -0600

    week-10

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Page() {
         <Link href="/week-7">Week 7 Assignments</Link>
         <Link href="/week-8">Week 8 Assignments</Link>
         <Link href="/week-9">Week 9 Assignments</Link>
-        <Link href="/week-9">Week 10 Assignments</Link>
+        <Link href="/week-10">Week 10 Assignments</Link>
       </p>
     </main>
   );
